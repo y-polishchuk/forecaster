4e19a64ab99184e1a500c6096447b45bad5b2c20
MandalorianFighter <yanush.polishchuk@gmail.com>
2024-06-18 12:50:53 +0300

    add an array of dummy data

--- src/Controller/WeatherController.php
+++ src/Controller/WeatherController.php
@@ -11,9 +11,45 @@ class WeatherController extends AbstractController
     #[Route('/weather/{countryCode}/{city}', name: 'app_weather')]
     public function forecast(string $countryCode, string $city): Response
     {
-        return $this->render('weather/index.html.twig', [
+        $forecasts = [
+            [
+                "date" => new \DateTime('2024-01-01'),
+                "temperatureCelsius" => 17,
+                "flTemperatureCelsius" => 16,
+                "pressure" => 1000,
+                "humidity" => 64,
+                "wind_speed" => 3.2,
+                "wind_deg" => 270,
+                "cloudiness" => 75,
+                "icon" => 'sun',
+            ],
+            [
+                "date" => new \DateTime('2024-01-02'),
+                "temperatureCelsius" => 12,
+                "flTemperatureCelsius" => 13,
+                "pressure" => 1001,
+                "humidity" => 62,
+                "wind_speed" => 2.2,
+                "wind_deg" => 180,
+                "cloudiness" => 50,
+                "icon" => 'cloud',
+            ],
+            [
+                "date" => new \DateTime('2024-01-03'),
+                "temperatureCelsius" => 11,
+                "flTemperatureCelsius" => 12,
+                "pressure" => 1010,
+                "humidity" => 63,
+                "wind_speed" => 3.5,
+                "wind_deg" => 270,
+                "cloudiness" => 15,
+                "icon" => 'cloud-rain',
+            ]
+        ];
+        return $this->render('weather/forecast.html.twig', [
             'country_code' => $countryCode,
-            'city' => $city
+            'city' => $city,
+            'forecasts' => $forecasts
         ]);
     }
 }
